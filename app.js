924f7623c2749d4e35a363dbd6010769f9552ec0
Guy Stitt <guyjstitt@gmail.com>
2015-01-31 19:36:43 -0500

    Completed AngularJS Code School Tutorial

---
+++ app.js
@@ -0,0 +1,26 @@
+(function() {
+	var app = angular.module('store', ['store-products']);
+
+	app.controller('StoreController',[ '$http', function($http) {
+		var store = this;
+
+		store.products = [];
+
+		$http.get('products.json').success(function(data) {
+			store.products = data;
+		});
+	}]);
+
+	app.controller("ReviewController", [ '$http', function($http) {
+		var review = this;
+
+		$http.get('products.json').success(function(data) {
+			review.products = data;
+		});
+
+		this.addReview = function(product) {
+			product.review.push(this.review);
+			this.review = {};
+		}
+	}]);
+})();
